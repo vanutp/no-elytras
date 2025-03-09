b99c464d5d40369c7329e7dd2bbeeb76b7f87c04
Ivan Filipenkov <hello@vanutp.dev>
2025-03-09 03:32:09 +0100

    edge cases + forbidden text

--- src/main/kotlin/dev/vanutp/no_elytras/NoElytras.kt
+++ src/main/kotlin/dev/vanutp/no_elytras/NoElytras.kt
@@ -1,18 +1,26 @@
 package dev.vanutp.no_elytras
 
+import com.destroystokyo.paper.event.player.PlayerElytraBoostEvent
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.context.CommandContext
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityToggleGlideEvent
+import org.bukkit.event.player.PlayerMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 
 class NoElytras : JavaPlugin(), Listener {
+    companion object {
+        val FORBIDDEN_TEXT = Component.text("Flying is not allowed here")
+            .color(NamedTextColor.BLUE)
+    }
     private var blacklistWorlds = listOf<String>()
 
     private fun reload() {
@@ -44,11 +52,28 @@ class NoElytras : JavaPlugin(), Listener {
         registerCommand()
     }
 
-    @EventHandler()
+    @EventHandler
     fun onElytra(e: EntityToggleGlideEvent) {
         val player = e.entity as? Player ?: return
         if (blacklistWorlds.contains(player.world.name)) {
             e.isCancelled = true
+            player.sendActionBar(FORBIDDEN_TEXT)
+        }
+    }
+
+    @EventHandler
+    fun onMove(e: PlayerMoveEvent) {
+        if (e.player.isGliding && blacklistWorlds.contains(e.player.world.name)) {
+            e.player.isGliding = false
+            e.player.sendActionBar(FORBIDDEN_TEXT)
+        }
+    }
+
+    @EventHandler
+    fun onBoost(e: PlayerElytraBoostEvent) {
+        if (blacklistWorlds.contains(e.player.world.name)) {
+            e.isCancelled = true
+            e.player.sendActionBar(FORBIDDEN_TEXT)
         }
     }
 }
